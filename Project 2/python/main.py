ba620cf6bb5f38d6b1b4a1c3dd73ce978a903a12
Unknown <mdkallick@gmail.com>
2017-10-08 18:55:41 -0400

    Added Plots and Answers
    Added a bunch of plots and finished the writeup. Still needs polishing.

--- Project 2/python/main.py
+++ Project 2/python/main.py
@@ -51,7 +51,7 @@ for i in range(1, 2):
     z_fin = sol[-1, 1]
     t_change = t[np.argwhere(X_star > 0)[0, 0]]
     t_mid = t[find_nearest(sol[:, 1], z_fin / 2.)]
-    print(t_mid - t_change)
+    print("response time of activator cascade: " + str(t_mid - t_change))
 
 """
 Feed-Forward Loop
@@ -83,7 +83,7 @@ tf = 3.5
 
 # Input variable
 for i in range(1, 2):
-    X_star = np.array([0] * int(.5/dt) + [i] * int(.5/dt) + [0] * int((2.5/dt) + 2)).T #+ [i] * int((2/dt)+2)).T
+    X_star = np.array([0] * int(.5 / dt) + [i] * int(.5 / dt) + [0] * int((2.5 / dt) + 2)).T  # + [i] * int((2/dt)+2)).T
     # X_star = np.array([0] * int(.5 / dt) + [i] * int((3 / dt) + 2)).T
 
     t, sol = ode15s(feed_forward, yinit, t0, dt, tf, params, inputs=X_star)
@@ -104,51 +104,66 @@ From: "A model for circadian oscillations in the Drosophila period protein"
 Goldbeter, A. : 1995
 """
 
-# parameters
-n = 4
-k1 = 2
-k2 = 2
-k3 = 2
-k4 = 2
-km = .5
-ks = .38
-kd = .2
-ki = 1
-v1 = 3.2
-v2 = 1.58
-v3 = 5
-v4 = 2.5
-vm = .65
-vd = .95
-vs = .76
-small_k1 = 1.9
-small_k2 = 1.3
-params = [n, k1, k2, k3, k4, km, ks, kd, ki, v1, v2, v3, v4, vm, vd, vs,
-          small_k1, small_k2]
 
-# initial conditions
-M = 1
-P0 = 1
-P1 = 1
-P2 = 1
-PN = 1
+def neg_feedback_loop(vd, vm):
+    # parameters
+    n = 4
+    k1 = 2
+    k2 = 2
+    k3 = 2
+    k4 = 2
+    km = .5
+    ks = .38
+    kd = .2
+    ki = 1
+    v1 = 3.2
+    v2 = 1.58
+    v3 = 5
+    v4 = 2.5
+    # vm = .65
+    # vd = .95
+    vs = .76
+    small_k1 = 1.9
+    small_k2 = 1.3
 
-yinit = [M, P0, P1, P2, PN]
+    params = [n, k1, k2, k3, k4, km, ks, kd, ki, v1, v2, v3, v4, vm, vd, vs,
+              small_k1, small_k2]
 
-t0 = 0
-dt = (72 / 3600)
-tf = 72
+    # initial conditions
+    M = .5
+    P0 = 1
+    P1 = .4
+    P2 = .4
+    PN = .4
 
-t, sol = ode15s(neg_feedback, yinit, t0, dt, tf, params)
+    yinit = [M, P0, P1, P2, PN]
 
-plt.plot(t, sol[:, 0], 'b', label='M')
-plt.plot(t, sol[:, 1], 'g', label='P0')
-plt.plot(t, sol[:, 2], 'm', label='P1')
-plt.plot(t, sol[:, 3], 'r', label='P2')
-plt.plot(t, sol[:, 4], 'k', label='PN')
-plt.legend(loc='best')
-plt.xlabel('time / h')
-plt.ylabel('PER forms or M')
-plt.title('Oscillations in PER over Time')
-plt.grid()
-plt.show()
+    t0 = 0
+    dt = (72 / 3600)
+    tf = 72
+
+    t, sol = ode15s(neg_feedback, yinit, t0, dt, tf, params)
+
+    j = 2
+
+    plt.plot(t[:-j], sol[:-j, 0], 'b', label='M')
+    plt.plot(t[:-j], sol[:-j, 1], 'g', label='P0')
+    plt.plot(t[:-j], sol[:-j, 2], 'm', label='P1')
+    plt.plot(t[:-j], sol[:-j, 3], 'r', label='P2')
+    plt.plot(t[:-j], sol[:-j, 4], 'k', label='PN')
+    plt.plot(t[:-j], np.sum(sol[:-j, 1:4], axis=1), 'c', label='PT')
+    plt.legend(loc='best')
+    plt.xlabel('time / h')
+    plt.ylabel('PER forms or M')
+    plt.ylim(ymin=0, ymax=5.5)
+    plt.title('Oscillations in PER over Time (vd = ' + str(vd) + '), (vm = ' + str(vm) + ')')
+    plt.grid()
+    plt.show()
+
+
+# vds = [.80, .85, .90, .95, 1.00, 1.05, 1.10]
+vds = [.95]
+vms = [.50, .55, .60, .65, .70, .75, .80]
+for i in range(len(vds)):
+    for j in range(len(vms)):
+        neg_feedback_loop(vds[i], vms[j])
