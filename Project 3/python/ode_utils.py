49613ba1faba6f351aeba9c5c5cd045c36609663
Unknown <mdkallick@gmail.com>
2017-10-26 13:48:33 -0400

    Added Predictive Time Steps
    Added an ode solving function that uses forward euler and explicit trapezoid methods to predictively size its time steps.

--- Project 3/python/ode_utils.py
+++ Project 3/python/ode_utils.py
@@ -77,6 +77,66 @@ def explicit_trapezoid(function, t, dt, prev, params):
     pred_step = function(t, forward_euler(function, t, dt, prev, params), params)
     return prev + (.5 * dt * (function(t, prev, params) + pred_step))
 
+def pred_ode_func(function, yinit, t0, tf, EST, params,
+					dt_init=float(tf-t0)/100., dt_out=None, inputs=None):
+	if (inputs is not None):
+        inputs = np.array([inputs])
+        params = np.array([params])
+        params = np.repeat(params, inputs.shape[1], axis=0).T
+        params = np.concatenate([params, inputs], axis=0)
+
+	t_cur = t0
+	t = [t_cur]
+	h = dt_init
+
+	if (dt_out != None):
+		t_out = np.arange(t0, tf + (dt_out), dt_out)
+		tmp_t = t_cur
+
+    sol = np.zeros((t.shape[0], len(yinit)))
+    sol[0] = yinit
+    sol_out = sol
+
+    i = 0
+    if (dt_out != None):
+	    j = 0
+    while (t_cur < tf):
+        if (inputs is None):
+            p = params
+        elif (inputs is not None):
+            p = params[i+1]
+
+		tmp_sol = forward_euler(function, t_cur, h, sol[i], p)
+		tmp_pred_sol = explicit_trapezoid(function, t_cur, h, sol[i], p)
+
+		err = np.linalg.norm(tmp_pred_sol - tmp_sol)
+
+		h_new = h * math.pow((.9 * EST/err), .5)
+		if (h_new > 2 * h):
+			h_new = 2 * h
+
+		if (err <= EST):
+	        sol[i+1] = tmp_sol
+	    	t_cur = t_cur + h
+	    	if(t_cur + h > tf):
+	    		h_new = tf - t_cur
+	    	t.append(t_cur)
+
+			if (dt_out != None):
+				while (tmp_t < t_cur):
+					slope = float(sol[i+1] - sol[i])/h
+					sol_out[j+1] = sol_out[j] + (dt_out * slope)
+					j = j + 1
+					tmp_t = tmp_t + dt_out
+
+
+		h = h_new
+
+    if (dt_out != None):
+		return t_out, sol_out
+	else:
+	    return t, sol
+
 def hill(X, K, n):
     return (math.pow(K, n) / (math.pow(K, n) + math.pow(X, n)))
 
