45532508a32eac5443164fc30449e084a4ffffaa
Unknown <mdkallick@gmail.com>
2017-11-03 00:02:44 -0400

    Finished Project 3
    Really could use some cleaning, but all of the utils and the odes work. The main file is a bit messy.

--- Project 3/python/main.py
+++ Project 3/python/main.py
@@ -1,7 +1,8 @@
+import time
 import numpy as np
 import matplotlib.pyplot as plt
 from odes import n_degrade, lotka_volterra
-from ode_utils import ode15s, find_nearest, ode_func
+from ode_utils import ode15s, find_nearest, ode_func, pred_ode_func
 
 """
 Degradation
@@ -20,38 +21,253 @@ Y0 = 1
 yinit = [X0, Y0]
 
 t0 = 0
-dts = np.arange(0.01, 0.09, 0.01)    
-tf = 10
+dt = .001
+tf = 100
+
+starttime = time.clock()
+t_rk4_deg, sol_rk4_deg = ode_func(n_degrade, yinit, t0, dt, tf, params, 'RK4')
+endtime = time.clock()
+print("rk4 runtime: {}".format(endtime-starttime))
+
+
+# parameters
+alpha = .25
+beta = .01
+gamma = 1
+delta = .01
+
+params = [alpha, beta, gamma, delta]
+
+# initial conditions
+X0 = 1
+Y0 = 1
+
+yinit = [X0, Y0]
+
+t0 = 0
+dt = .01
+
+tf = 100
+
+t_rk4_lv, sol_rk4_lv = ode_func(lotka_volterra, yinit, t0, dt, tf, params, 'RK4')
+
+
+
+f, axs = plt.subplots(1, 2, figsize=(8, 4), sharey=False)
+f.suptitle('Runge-Kutta 4')
+axs[0].plot(t_rk4_deg, sol_rk4_deg[:,0], 'b', label='X')
+axs[0].plot(t_rk4_deg, sol_rk4_deg[:,1], 'm', label='Y')
+axs[0].legend(loc='best')
+axs[0].set_title('Degradation')
+axs[0].set_xlabel('time (s)')
+axs[0].set_ylabel('concentrations (nM)')
+axs[1].plot(t_rk4_lv, sol_rk4_lv[:,0], 'b', label='X')
+axs[1].plot(t_rk4_lv, sol_rk4_lv[:,1], 'm', label='Y')
+axs[1].legend(loc='best')
+axs[1].set_title('Lotka-Volterra')
+axs[1].set_xlabel('time (s)')
+axs[1].set_ylabel('concentrations (nM)')
+plt.grid()
+plt.show()
+
+# considered "correct", so very small dt
+t, sol = ode15s(n_degrade, yinit, t0, .001, tf, params)
+
+ts = []
+sols = []
+
 for dt in dts:
-  t_fe, sol_fe = ode_func(n_degrade, yinit, t0, dt, tf, params, 'forward_euler')
-  t_et, sol_et = ode_func(n_degrade, yinit, t0, dt, tf, params, 'explicit_trapezoid')
+    starttime = time.clock()
+    t_fe, sol_fe = ode_func(n_degrade, yinit, t0, dt, tf, params, 'explicit_trapezoid')
+    endtime = time.clock()
+    ts.append(t_fe)
+    sols.append(sol_fe)
+    print("forward euler runtime: {}".format(endtime-starttime))
+    # starttime = time.clock()
+    # t_et, sol_et = ode_func(lotka_volterra, yinit, t0, dt, tf, params, 'explicit_trapezoid')
+    # endtime = time.clock()
+    # print("explicit trapezoid runtime: {}".format(endtime-starttime))
+    # starttime = time.clock()
+    # t_rk4, sol_rk4 = ode_func(lotka_volterra, yinit, t0, dt, tf, params, 'RK4')
+    # endtime = time.clock()
+    # print("RK4 runtime: {}".format(endtime-starttime))
 
-  # considered "correct", so very small dt
-  t, sol = ode15s(n_degrade, yinit, t0, .001, tf, params)
+f, axs = plt.subplots(2, 3, figsize=(9, 9), sharey=True)
+i, j, k = [0, 0, 0]
+for sol in sols:
+    f.suptitle('Explicit Trapezoid with Varying dt')
+    cur = j + i
+    axs[k][i].plot(ts[cur], sol[:,0], 'g', label='X')
+    axs[k][i].plot(ts[cur], sol[:,1], 'm', label='Y')
+    # axs[k][i].plot(ts[cur], np.abs(np.subtract(sol[:,0], tss[cur][:,0])),
+    #                                                     'r-', label='x error')
+    # axs[k][i].plot(ts[cur], np.abs(np.subtract(sol[:,1], tss[cur][:,1])),
+    #                                                     'b-', label='y error')
+    axs[k][i].legend(loc='best')
+    axs[k][i].set_xlabel('time (s)')
+    axs[k][i].set_ylabel('concentrations (nM)')
+    axs[k][i].set_title('dt = ' + str(dts[cur]))
+    axs[k][i].grid()
+    if(i == 2):
+        j = j + 3
+        i = 0
+        k = k + 1
+    else:
+        i = i + 1
 
-  d = np.abs(sol-sol_fe)
-  avg_err = mean(d)
+plt.show()
 
-f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
-ax1.plot(t_fe, sol_fe[:, 0], 'g', label='X')
-ax1.plot(t_fe, sol_fe[:, 1], 'm', label='Y')
+"""
+Lotka Volterra
+"""
+"""
+# parameters
+alpha = .25
+beta = .01
+gamma = 1
+delta = .01
+
+params = [alpha, beta, gamma, delta]
+
+# initial conditions
+X0 = 1
+Y0 = 1
+
+yinit = [X0, Y0]
+
+t0 = 0
+dts = np.arange(0.01, .13, 0.02)
+tf = 100
+
+# considered "correct", so very small dt
+t, sol = ode15s(lotka_volterra, yinit, t0, .001, tf, params)
+
+ts = []
+sols = []
+tts = []
+tss = []
+
+for dt in dts:
+    starttime = time.clock()
+    t_fe, sol_fe = ode_func(lotka_volterra, yinit, t0, dt, tf, params, 'explicit_trapezoid')
+    true_t, true_s = ode15s(lotka_volterra, yinit, t0, dt, tf, params)
+    endtime = time.clock()
+    ts.append(t_fe)
+    sols.append(sol_fe)
+    tts.append(true_t[:-1])
+    tss.append(true_s[:-1])
+    print("forward euler runtime: {}".format(endtime-starttime))
+    # starttime = time.clock()
+    # t_et, sol_et = ode_func(lotka_volterra, yinit, t0, dt, tf, params, 'explicit_trapezoid')
+    # endtime = time.clock()
+    # print("explicit trapezoid runtime: {}".format(endtime-starttime))
+    # starttime = time.clock()
+    # t_rk4, sol_rk4 = ode_func(lotka_volterra, yinit, t0, dt, tf, params, 'RK4')
+    # endtime = time.clock()
+    # print("RK4 runtime: {}".format(endtime-starttime))
+
+
+
+	# d = np.abs(sol-sol_fe)
+	#   avg_err = mean(d)
+
+f, axs = plt.subplots(2, 3, figsize=(9, 9), sharey=True)
+i, j, k = [0, 0, 0]
+for sol in sols:
+    f.suptitle('Explicit Trapezoid with Varying dt')
+    cur = j + i
+    axs[k][i].plot(ts[cur], sol[:,0], 'g', label='X')
+    axs[k][i].plot(ts[cur], sol[:,1], 'm', label='Y')
+    # axs[k][i].plot(ts[cur], np.abs(np.subtract(sol[:,0], tss[cur][:,0])),
+    #                                                     'r-', label='x error')
+    # axs[k][i].plot(ts[cur], np.abs(np.subtract(sol[:,1], tss[cur][:,1])),
+    #                                                     'b-', label='y error')
+    axs[k][i].legend(loc='best')
+    axs[k][i].set_xlabel('time (s)')
+    axs[k][i].set_ylabel('concentrations (nM)')
+    axs[k][i].set_title('dt = ' + str(dts[cur]))
+    axs[k][i].grid()
+    if(i == 2):
+        j = j + 3
+        i = 0
+        k = k + 1
+    else:
+        i = i + 1
+
+plt.show()
+"""
+
+"""
+Testing function for predictive time steps
+"""
+# parameters
+alpha = .1
+beta = .2
+
+params = [alpha, beta]
+
+# initial conditions
+X0 = 1
+Y0 = 1
+
+yinit = [X0, Y0]
+
+t0 = 0
+tf = 10
+
+ests = [5e-1, 5e-2, 5e-3, 5e-4, 5e-5, 5e-6]
+
+t_pres = []
+s_pres = []
+for est in ests:
+    t_pre, sol_pre = pred_ode_func(n_degrade, yinit, t0, tf, est, params)#,
+                                #dt_out=.1)
+    t_pres.append(t_pre)
+    s_pres.append(sol_pre)
+
+f, axs = plt.subplots(2, 3, figsize=(12, 8), sharey=True)
+i, j, k = [0, 0, 0]
+f.suptitle('Size of Predictive Time-Steps with Varying Error Tolerance')
+for t_pre in t_pres:
+    cur = j + i
+    axs[k][i].plot(t_pre[:-1], np.diff(t_pre), 'g', label='X')
+    axs[k][i].legend(loc='best')
+    axs[k][i].set_xlabel('time (s)')
+    axs[k][i].set_ylabel('time step size (s)')
+    axs[k][i].set_title('EST = ' + str(ests[cur]))
+    axs[k][i].grid()
+    if(i == 2):
+        j = j + 3
+        i = 0
+        k = k + 1
+    else:
+        i = i + 1
+f.show()
+
+# plt.plot(t_pre[:-1], dt_pre, 'b', label='time step size')
+# plt.legend(loc='best')
+# plt.xlabel('time (s)')
+# plt.ylabel('time step size (s)')
+# plt.title('Size of time steps')
+# plt.grid()
+# plt.show()
+
+# considered "correct", so very small dt
+t, sol = ode15s(n_degrade, yinit, t0, .001, tf, params)
+
+f, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4), sharey=True)
+ax1.plot(t_pre, sol_pre[:, 0], 'g', label='X')
+ax1.plot(t_pre, sol_pre[:, 1], 'm', label='Y')
 ax1.legend(loc='best')
 ax1.set_xlabel('time (s)')
 ax1.set_ylabel('concentrations (nM)')
-ax1.set_title('Degradation (forward_euler)')
+ax1.set_title('Degradation (predictive time steps)')
 ax1.grid()
-ax2.plot(t_et, sol_et[:, 0], 'g', label='X')
-ax2.plot(t_et, sol_et[:, 1], 'm', label='Y')
+ax2.plot(t, sol[:, 0], 'g', label='X')
+ax2.plot(t, sol[:, 1], 'm', label='Y')
 ax2.legend(loc='best')
 ax2.set_xlabel('time (s)')
 ax2.set_ylabel('concentrations (nM)')
-ax2.set_title('Degradation (explicit_trapezoid)')
+ax2.set_title('Degradation (ode15s)')
 ax2.grid()
-ax3.plot(t, sol[:, 0], 'g', label='X')
-ax3.plot(t, sol[:, 1], 'm', label='Y')
-ax3.legend(loc='best')
-ax3.set_xlabel('time (s)')
-ax3.set_ylabel('concentrations (nM)')
-ax3.set_title('Degradation (ode15s)')
-ax3.grid()
 plt.show()
--- Project 3/python/ode_utils.py
+++ Project 3/python/ode_utils.py
@@ -10,11 +10,11 @@ every timestep.
 
 
 def ode15s(function, yinit, t0, dt, tf, params, inputs=None):
-    t = np.arange(t0, tf + (dt * 2), dt)    
-    
+    t = np.arange(t0, tf + (dt * 2), dt)
+
     if (inputs is not None):
         inputs = np.array([inputs])
-        if(inputs.shape[0] != t.shape[0]):
+        if (inputs.shape[0] != t.shape[0]):
             raise Exception("Size of input matrix does not match the number of time steps")
         params = np.array([params])
         params = np.repeat(params, inputs.shape[1], axis=0).T
@@ -23,8 +23,6 @@ def ode15s(function, yinit, t0, dt, tf, params, inputs=None):
 
     if (inputs is None):
         r = set_ode15s(function, yinit, t0, params)
-
-
 
     sol = np.zeros((t.shape[0], len(yinit)))
     sol[0] = yinit
@@ -47,17 +45,22 @@ def ode15s(function, yinit, t0, dt, tf, params, inputs=None):
 def set_ode15s(function, yinit, t0, params):
     return ode(function).set_f_params(params).set_initial_value(yinit, t0).set_integrator('vode', method='bdf', order=5)
 
+
 def ode_func(function, yinit, t0, dt, tf, params, method, inputs=None):
     if (inputs is not None):
         inputs = np.array([inputs])
         params = np.array([params])
         params = np.repeat(params, inputs.shape[1], axis=0).T
         params = np.concatenate([params, inputs], axis=0)
-        
+
     if (method == 'forward_euler'):
         stepfunc = forward_euler
     elif (method == 'explicit_trapezoid'):
         stepfunc = explicit_trapezoid
+    elif (method == 'RK4'):
+        stepfunc = RK4
+    else:
+        raise Exception("Step Function requested does not match any implemented functions")
 
     t = np.arange(t0, tf + (dt), dt)
 
@@ -69,78 +72,96 @@ def ode_func(function, yinit, t0, dt, tf, params, method, inputs=None):
         if (inputs is None):
             p = params
         elif (inputs is not None):
-            p = params[i+1]
+            p = params[i + 1]
 
-        sol[i+1] = stepfunc(function, t, dt, sol[i], p)
-        
+        sol[i + 1] = stepfunc(function, t, dt, sol[i].tolist(), p)
+
     return t, sol
 
+
 def forward_euler(function, t, dt, prev, params):
-    return prev + (dt * function(t, prev, params))
+    return prev + (dt * np.array(function(t, prev, params)))
+
 
 def explicit_trapezoid(function, t, dt, prev, params):
     pred_step = function(t, forward_euler(function, t, dt, prev, params), params)
-    return prev + (.5 * dt * (function(t, prev, params) + pred_step))
+    return prev + (.5 * dt * np.add(function(t, prev, params), pred_step))
+
+def RK4(function, t, dt, prev, params):
+    S0 = prev
+    F1 = np.array(function(t, S0, params))
+    S1 = prev + ((dt / 2) * F1)
+    F2 = np.array(function(t + (dt / 2), S1, params))
+    S2 = prev + ((dt / 2) * F2)
+    F3 = np.array(function(t + (dt / 2), S2, params))
+    S3 = prev + (dt * F3)
+    F4 = np.array(function(t + dt, S3, params))
+    cur = prev + ((dt / 6) * (F1 + (2 * F2) + (2 * F3) + F4))
+    return cur
 
 def pred_ode_func(function, yinit, t0, tf, EST, params,
-					dt_init=float(tf-t0)/100., dt_out=None, inputs=None):
-	if (inputs is not None):
+                  dt_init=None, dt_out=None, inputs=None):
+    if (dt_init == None):
+        dt_init = float(tf - t0) / 100.
+    if (inputs is not None):
         inputs = np.array([inputs])
         params = np.array([params])
         params = np.repeat(params, inputs.shape[1], axis=0).T
         params = np.concatenate([params, inputs], axis=0)
 
-	t_cur = t0
-	t = [t_cur]
-	h = dt_init
+    t_cur = t0
+    t = [t_cur]
+    h = dt_init
 
-	if (dt_out != None):
-		t_out = np.arange(t0, tf + (dt_out), dt_out)
-		tmp_t = t_cur
+    if (dt_out != None):
+        # t_out = np.arange(t0, tf + (dt_out), dt_out)
+        t_out = [t0]
+        tmp_t = t_cur
 
-    sol = np.zeros((t.shape[0], len(yinit)))
-    sol[0] = yinit
-    sol_out = sol
+    sol = np.array(yinit, ndmin=2)
+    sol_out = np.array(sol, ndmin=2)
 
     i = 0
     if (dt_out != None):
-	    j = 0
+        j = 0
     while (t_cur < tf):
         if (inputs is None):
             p = params
         elif (inputs is not None):
-            p = params[i+1]
+            p = params[i + 1]
 
-		tmp_sol = forward_euler(function, t_cur, h, sol[i], p)
-		tmp_pred_sol = explicit_trapezoid(function, t_cur, h, sol[i], p)
+        tmp_sol = forward_euler(function, t_cur, h, sol[i], p)
+        tmp_pred_sol = explicit_trapezoid(function, t_cur, h, sol[i], p)
 
-		err = np.linalg.norm(tmp_pred_sol - tmp_sol)
+        err = np.linalg.norm(tmp_pred_sol - tmp_sol)
 
-		h_new = h * math.pow((.9 * EST/err), .5)
-		if (h_new > 2 * h):
-			h_new = 2 * h
+        h_new = h * math.pow((.9 * EST / err), .5)
+        if (h_new > (2 * h)):
+            h_new = (2 * h)
 
-		if (err <= EST):
-	        sol[i+1] = tmp_sol
-	    	t_cur = t_cur + h
-	    	if(t_cur + h > tf):
-	    		h_new = tf - t_cur
-	    	t.append(t_cur)
+        if (err <= EST):
+            sol = np.concatenate([sol, np.array(tmp_sol, ndmin=2)], axis=0)
+            t_cur = t_cur + h
+            if (t_cur + h_new > tf):
+                h_new = tf - t_cur
+            t.append(t_cur)
+            i = i + 1
 
-			if (dt_out != None):
-				while (tmp_t < t_cur):
-					slope = float(sol[i+1] - sol[i])/h
-					sol_out[j+1] = sol_out[j] + (dt_out * slope)
-					j = j + 1
-					tmp_t = tmp_t + dt_out
+            if (dt_out != None):
+                while (tmp_t < t_cur - dt_out):
+                    slope = np.divide((np.array(sol[i + 1]) - np.array(sol[i])), h)
+                    tmpnxt = np.array(sol_out[j] + (dt_out * slope), ndmin=2)
+                    sol_out = np.concatenate([sol_out, tmpnxt], axis=0)
+                    j = j + 1
+                    tmp_t = tmp_t + dt_out
+                    t_out.append(tmp_t)
 
-
-		h = h_new
+        h = h_new
 
     if (dt_out != None):
-		return t_out, sol_out
-	else:
-	    return t, sol
+        return np.array(t_out), np.array(sol_out)
+    else:
+        return np.array(t), np.array(sol)
 
 def hill(X, K, n):
     return (math.pow(K, n) / (math.pow(K, n) + math.pow(X, n)))
